e1593fef33b41829423fa98349ab2e68dc4e62b2
Abdelrhman Walaa <44446382+AbdelrhmanWalaa@users.noreply.github.com>
2022-08-11 19:47:33 +0200

    Update DIO_Config.h

--- DIO_Driver/DIO_Config.h
+++ DIO_Driver/DIO_Config.h
@@ -11,29 +11,29 @@
  
 /* Pinouts ATmega32(L): */
 /*
-							  PDIP
-						  -------------
+					PDIP
+				    -------------
 		   (XCK/T0) PB0 -| 1		40 |- PA0 (ADC0)
-			   (T1)	PB1	-| 2		39 |- PA1 (ADC1)
-		(INT2/AIN0)	PB2	-| 3		38 |- PA2 (ADC2)
-		 (OC0/AIN1)	PB3	-| 4		37 |- PA3 (ADC3)
-			   (SS)	PB4	-| 5		36 |- PA4 (ADC4)
-			 (MOSI)	PB5	-| 6		35 |- PA5 (ADC5)
-			 (MISO)	PB6	-| 7		34 |- PA6 (ADC6)
-			  (SCK)	PB7	-| 8		33 |- PA7 (ADC7)
-				  RESET -| 9		32 |- AREF
-					VCC	-| 10		31 |- GND
-					GND	-| 11		30 |- AVCC
-				  XTAL2	-| 12		29 |- PC0 (TOSC2)
-				  XTAL1	-| 13		28 |- PC1 (TOSC1)
-			  (RXD)	PD0 -| 14		27 |- PC2 (TDI)
-			  (TXD)	PD1 -| 15		26 |- PC3 (TDO)
-			 (INT0)	PD2 -| 16		25 |- PC4 (TMS)
-			 (INT1)	PD3 -| 17		24 |- PC5 (TCK)
-			 (OC1B)	PD4 -| 18		23 |- PC6 (SDA)
-			 (OC1A)	PD5 -| 19		22 |- PC7 (SCL)
-			  (ICP)	PD6 -| 20		21 |- PD7 (OC2)
-					 	  -------------
+		       (T1) PB1 -| 2		39 |- PA1 (ADC1)
+	        (INT2/AIN0) PB2 -| 3		38 |- PA2 (ADC2)
+	         (OC0/AIN1) PB3 -| 4		37 |- PA3 (ADC3)
+		       (SS) PB4	-| 5		36 |- PA4 (ADC4)
+		     (MOSI) PB5	-| 6		35 |- PA5 (ADC5)
+		     (MISO) PB6	-| 7		34 |- PA6 (ADC6)
+		      (SCK) PB7	-| 8		33 |- PA7 (ADC7)
+			  RESET -| 9		32 |- AREF
+			    VCC	-| 10		31 |- GND
+			    GND	-| 11		30 |- AVCC
+			  XTAL2	-| 12		29 |- PC0 (TOSC2)
+			  XTAL1	-| 13		28 |- PC1 (TOSC1)
+		      (RXD) PD0 -| 14		27 |- PC2 (TDI)
+		      (TXD) PD1 -| 15		26 |- PC3 (TDO)
+		     (INT0) PD2 -| 16		25 |- PC4 (TMS)
+		     (INT1) PD3 -| 17		24 |- PC5 (TCK)
+		     (OC1B) PD4 -| 18		23 |- PC6 (SDA)
+		     (OC1A) PD5 -| 19		22 |- PC7 (SCL)
+		      (ICP) PD6 -| 20		21 |- PD7 (OC2)
+				    -------------
  */
 
 /* DIO Configurations' Definitions */
@@ -43,9 +43,9 @@
 
 /* Initial Pin Values */
 #define DIO_u8_INPUT_FLOATING				0
-#define DIO_u8_INPUT_PULLUP_RESISTOR		1
-#define DIO_u8_OUTPUT_LOW					0
-#define DIO_u8_OUTPUT_HIGH					1
+#define DIO_u8_INPUT_PULLUP_RESISTOR			1
+#define DIO_u8_OUTPUT_LOW				0
+#define DIO_u8_OUTPUT_HIGH				1
 /* End of Configurations' Definitions */
 
 /* DIO Configurations */
@@ -55,9 +55,9 @@
  */
 
 /* PORTA */
-#define DIO_u8_PA0_INITIAL_DIRECTION		DIO_u8_INITIAL_INPUT
-#define DIO_u8_PA1_INITIAL_DIRECTION		DIO_u8_INITIAL_INPUT
-#define DIO_u8_PA2_INITIAL_DIRECTION		DIO_u8_INITIAL_INPUT
+#define DIO_u8_PA0_INITIAL_DIRECTION	    DIO_u8_INITIAL_INPUT
+#define DIO_u8_PA1_INITIAL_DIRECTION	    DIO_u8_INITIAL_INPUT
+#define DIO_u8_PA2_INITIAL_DIRECTION	    DIO_u8_INITIAL_INPUT
 #define DIO_u8_PA3_INITIAL_DIRECTION        DIO_u8_INITIAL_INPUT
 #define DIO_u8_PA4_INITIAL_DIRECTION        DIO_u8_INITIAL_INPUT
 #define DIO_u8_PA5_INITIAL_DIRECTION        DIO_u8_INITIAL_INPUT
@@ -102,9 +102,9 @@
  */
 
 /* PORTA */
-#define DIO_u8_PA0_INITIAL_VALUE			DIO_u8_INPUT_FLOATING
-#define DIO_u8_PA1_INITIAL_VALUE			DIO_u8_INPUT_FLOATING
-#define DIO_u8_PA2_INITIAL_VALUE			DIO_u8_INPUT_FLOATING
+#define DIO_u8_PA0_INITIAL_VALUE		DIO_u8_INPUT_FLOATING
+#define DIO_u8_PA1_INITIAL_VALUE		DIO_u8_INPUT_FLOATING
+#define DIO_u8_PA2_INITIAL_VALUE		DIO_u8_INPUT_FLOATING
 #define DIO_u8_PA3_INITIAL_VALUE	        DIO_u8_INPUT_FLOATING
 #define DIO_u8_PA4_INITIAL_VALUE	        DIO_u8_INPUT_FLOATING
 #define DIO_u8_PA5_INITIAL_VALUE	        DIO_u8_INPUT_FLOATING
